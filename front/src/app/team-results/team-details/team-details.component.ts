c4ec91dd89e6e81ee13ecb188aaa97b7ee849684
adrien zakian <azakian@luccasoftware.com>
2026-09-22 23:09:53 +0200

    fix(detail): last game scores ui in phone device display

--- front/src/app/team-results/team-details/team-details.component.ts
+++ front/src/app/team-results/team-details/team-details.component.ts
@@ -1,10 +1,10 @@
 import { Component, computed, inject, input } from '@angular/core';
-import { TeamId } from '../shared/teamId';
-import { TeamDetailsRepository } from './team-details.repository';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 import { SkeletonComponent } from '../component/skeleton';
 import { Game } from '../shared/game';
-import { RouterLink } from '@angular/router';
+import { TeamId } from '../shared/teamId';
+import { TeamDetailsRepository } from './team-details.repository';
 
 @Component({
   imports: [SkeletonComponent, RouterLink],
@@ -33,38 +33,37 @@ export class TeamDetailsComponent {
   });
 
   private getResults(teamName: string, game: Game) {
-    const _isTeam1 =
-      teamName.localeCompare(game.team1Name, undefined, { sensitivity: 'base' }) === 0;
-    const teamScore = _isTeam1 ? game.scores!.team1Score : game.scores!.team2Score;
-    const opponent = _isTeam1 ? game.team2Name : game.team1Name;
-    const opponentScore = _isTeam1 ? game.scores!.team2Score : game.scores!.team1Score;
+    const isHome = teamName.localeCompare(game.team1Name, undefined, { sensitivity: 'base' }) === 0;
+    const teamScore = isHome ? game.scores!.team1Score : game.scores!.team2Score;
+    const opponent = isHome ? game.team2Name : game.team1Name;
+    const opponentScore = isHome ? game.scores!.team2Score : game.scores!.team1Score;
     const isWin = teamScore > opponentScore;
 
-    const _validSets = [
+    const validSets = [
       game.setsScore!.set1,
       game.setsScore!.set2,
       game.setsScore!.set3,
       game.setsScore!.set4,
       game.setsScore!.set5,
     ].filter((set): set is string => Boolean(set));
-
-    const teamSetScores = _validSets.map((set) => {
-      const [score1, score2] = set.split('-').map(Number);
-      return _isTeam1 ? score1 : score2;
-    });
-
-    const opponentSetScores = _validSets.map((set) => {
-      const [score1, score2] = set.split('-').map(Number);
-      return _isTeam1 ? score2 : score1;
-    });
+    //
+    // const teamSetScores = _validSets.map((set) => {
+    //   const [score1, score2] = set.split(':');
+    //   return isHome ? score1 : score2;
+    // });
+    //
+    // const opponentSetScores = _validSets.map((set) => {
+    //   const [score1, score2] = set.split(':');
+    //   return isHome ? score2 : score1;
+    // });
 
     return {
+      isHome,
       opponent,
       teamScore,
       opponentScore,
       isWin,
-      teamSetScores,
-      opponentSetScores,
+      validSets,
       date: game.date,
     };
   }
